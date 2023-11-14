7cda5be2c4eecc958943a1ecd11cdaafce730865
이용재 <80679257+dydwo0740@users.noreply.github.com>
2023-11-14 13:48:00 +0900

    feat: Content를 위한 Dto생성

--- forum/src/main/java/forum/forum/dto/ContentDto.java
+++ forum/src/main/java/forum/forum/dto/ContentDto.java
@@ -1,4 +1,11 @@
 package forum.forum.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 public class ContentDto {
+    private String writer;
+    private String title;
+    private String content;
 }
