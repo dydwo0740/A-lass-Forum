bfd70cadd32ded29a0e74ea8fb7720b20770c80d
이용재 <80679257+dydwo0740@users.noreply.github.com>
2023-11-14 13:46:26 +0900

    feat: Content와 Comment 연관관계 매핑

--- forum/src/main/java/forum/forum/entity/Content.java
+++ forum/src/main/java/forum/forum/entity/Content.java
@@ -1,27 +1,37 @@
 package forum.forum.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import lombok.Builder;
-import lombok.Getter;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
 public class Content {
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "content_id")
     private Long id;
     private String writer;
     private String title;
     private String content;
 
-    private LocalDateTime saveDate;
+    private LocalDateTime saveDate; // 작성한 시간 저장하고 싶다
+
+    @OneToMany(mappedBy = "mainContent")
+    private List<Comment> comments = new ArrayList<>();
+
+    public void addComment(Comment comment) {
+        this.comments.add(comment);
+        comment.setMainContent(this);
+    }
 
     @Builder
-    public Content(Long id, String writer, String title, String content) {
-        this.id = id;
+    public Content(String writer, String title, String content) {
         this.writer = writer;
         this.title = title;
         this.content = content;
