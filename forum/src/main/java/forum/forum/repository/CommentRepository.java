68ff70e0d0abe292aeaf97d2d6fed47329df5762
이용재 <80679257+dydwo0740@users.noreply.github.com>
2023-11-14 19:02:31 +0900

    feat: 댓글 수정을 위한 쿼리 생성

--- forum/src/main/java/forum/forum/repository/CommentRepository.java
+++ forum/src/main/java/forum/forum/repository/CommentRepository.java
@@ -2,11 +2,18 @@ package forum.forum.repository;
 
 import forum.forum.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     public List<Comment> findByMainContentId(Long contentId);
+    @Modifying
+    @Transactional
+    @Query("UPDATE Comment c SET c.content = :newContent, c.writer = :newWriter WHERE c.id = :id")
+    void updateContentAndWriterById(@Param("id") Long id, @Param("newContent") String newContent, @Param("newWriter") String newWriter);
+
 }
